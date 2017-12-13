df9dbe01033f7c47af5342bae96b0e81a5e18179
GIP2000 <gpress2222@gmail.com>
2017-12-13 10:41:34 -0500

    Functioning Editor page v1
    Added a functional editor page and outlined where the cards go.

--- myquill.js
+++ myquill.js
@@ -1,7 +1,23 @@
+
+
+
 $(document).ready(function(){
+	var toolbarOptions = [ 
+	[{'font' : []}],
+	[ {'size': []}],
+	['bold','italic','underline'],
+	[{"color": []}, {"background": []}], 
+	['link','video','formula','image'],
+	[{'align':[]}],
+	[{'indent': '+1'}, {'indent': '-1'}	]
+	];
 	
-	 var quill = new Quill('#editor', {
-      theme: 'snow'
-    });
+	var quill = new Quill('#editor', 	{
+		modules: { toolbar: toolbarOptions}, 
+		theme: "snow"
+	});
+	
+
+	
 	
 });
